089d828e1d2ba7a5409f7a72757d9d104e6ea3ae
Agboola Oluwatofunmi <oluwatofunmiagboola1@gmail.com>
2025-10-17 01:02:39 +0100

    commit number 4

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -21,7 +21,7 @@ const SongCard = ({ song, i, isPlaying, activeSong, data }) => {
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div
-          onClick={() => console.log(song)}
+          onClick={handlePlayClick}
           className={`absolute inset-0 justify-center items-center bg-black opacity-50 group-hover:flex ${
             activeSong?.attributes?.name === song.attributes?.name
               ? "flex bg-black opacity-50"
